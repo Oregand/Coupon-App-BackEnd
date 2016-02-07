a1efccefb3fa39e02c942af394393ca384464d6c
Graham Whelan <gawhelan@gmail.com>
2016-02-07 16:23:59 +0000

    Add basic logging of requests

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 var koa = require('koa');
+var logger = require('koa-logger');
 
 var config = require('./config');
 
 var app = koa();
 
+app.use(logger());
 app.use(require('./routes')(config));
 
 app.listen(config.port);
