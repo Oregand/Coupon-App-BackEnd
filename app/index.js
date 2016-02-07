73dc9d3318e2eccabc81a17017873268cedf7f3e
Graham Whelan <gawhelan@gmail.com>
2016-02-07 16:44:06 +0000

    Enable CORS support in development server

--- app/index.js
+++ app/index.js
@@ -3,9 +3,15 @@ var logger = require('koa-logger');
 
 var config = require('./config');
 
+
 var app = koa();
 
 app.use(logger());
+
+if (config.env === 'development') {
+    app.use(require('kcors')());
+}
+
 app.use(require('./routes')(config));
 
 app.listen(config.port);
